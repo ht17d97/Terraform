3c2f50b114a4877cfe5d61b8a5e71b981227458a
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-19 12:39:10 +0530

    Update provider.tf

--- Azure/Users/Role-Assign/provider.tf
+++ Azure/Users/Role-Assign/provider.tf
@@ -1,12 +1,27 @@
-variable "tenant_id" {
-  type        = string
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "3.1.0"
+    }
+  }
 }
-variable "subscription_id" {
-  type        = string
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  features {
+  }
 }
-variable "client_id" {
-  type        = string
-}
-variable "client_secret" {
-  type        = string
+
+provider "azuread" {
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
