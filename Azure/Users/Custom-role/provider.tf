6c04832c590fe2f80787d75b7beb6a7ab564849d
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-19 13:19:16 +0530

    Update provider.tf

--- Azure/Users/Custom-role/provider.tf
+++ Azure/Users/Custom-role/provider.tf
@@ -1,1 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "3.1.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  subscription_id = 
+  tenant_id       = 
+  client_id       = 
+  client_secret   = 
+  features {
+  }
+}
+
+provider "azuread" {
+  tenant_id       = 
+  client_id       = 
+  client_secret   = 
+}
