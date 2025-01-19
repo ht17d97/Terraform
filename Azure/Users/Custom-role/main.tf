dfda4d0d8a73805eb5ca5a2f9ec6e9c23a1844ff
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-19 13:18:41 +0530

    Update main.tf

--- Azure/Users/Custom-role/main.tf
+++ Azure/Users/Custom-role/main.tf
@@ -1,1 +1,36 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "testing-usercreate"
+  location = "South India"
+}
+resource "azuread_user" "example" {
+  user_principal_name = "Ram@himanshutariyal2025outlook.onmicrosoft.com"
+  display_name        = "ram"
+  mail_nickname       = "ram"
+  password            = "SecretP@sswd99!"
+}
+data "azurerm_subscription" "primary" {
+}
+
+resource "azurerm_role_definition" "example" {
+  name        = "custom-vm-on"
+  scope       = data.azurerm_subscription.primary.id
+  description = "This is a custom role created via Terraform"
+
+  permissions {
+    actions     = [
+    "Microsoft.Compute/*/read",
+    "Microsoft.Compute/virtualMachines/start/action",
+    "Microsoft.Compute/virtualMachines/restart/action",
+    ]
+    not_actions = []
+  }
+  assignable_scopes = [
+    data.azurerm_subscription.primary.id,
+  ]
+}
+resource "azurerm_role_assignment" "custom-vm-on-assignment" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "custom-vm-on"
+  principal_id         = azuread_user.example.object_id
+}
 
