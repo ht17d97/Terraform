c82706bab3ff13560e49c04ecb7bb25a1b1b432f
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-31 00:53:40 +0530

    Create terraform.tf

---
+++ Azure/Module/Multi-VM-creation/terraform.tf
@@ -0,0 +1,12 @@
+rg_name       = "Data-RG"
+rg_location   = "eastus"
+vnet_name     = "mgmt-network"
+subnet_range  = ["10.0.0.0/16"]
+subnet_name   = "mgmt-subnet"
+address_pre   = ["10.0.2.0/24"]
+nic           = "VM-nic"
+nsg_name      = "mgmt-nsg-rule"
+vm_name       = "mgmt-prod"
+vm_size       = "Standard_F2"
+vmcountnumber = 2
+public_ip_vm  = "mgmt-public-ip"
