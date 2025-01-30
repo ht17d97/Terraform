5b2f7d6415cf7def5f7ad1cfbbfc6d3a6cee387b
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-30 22:10:04 +0530

    Create variable.tf

---
+++ Azure/Module/VM-creation/variable.tf
@@ -0,0 +1,30 @@
+variable "rg_name" {
+  type        = string
+}
+variable "rg_location" {
+  type        = string
+}
+variable "vnet_name" {
+  type        = string
+}
+variable "subnet_range" {
+  type        = list(any)
+}
+variable "subnet_name" {
+  type        = string
+}
+variable "address_pre" {
+  type        = list(any)
+}
+variable "nic" {
+  type        = string
+}
+variable "nsg_name" {
+  type        = string
+}
+variable "vm_name" {
+  type        = string
+}
+variable "vm_size" {
+  type        = string
+}
