2fdc1cac00f43cf2134c6df998c570a208c6db83
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-31 00:52:17 +0530

    Create main.tf

---
+++ Azure/Module/Multi-VM-creation/main.tf
@@ -0,0 +1,15 @@
+module "rg" {
+  source        = "./module_rg"
+  rg_name       = var.rg_name
+  rg_location   = var.rg_location
+  vnet_name     = var.vnet_name
+  subnet_range  = var.subnet_range
+  subnet_name   = var.subnet_name
+  address_pre   = var.address_pre
+  nic           = var.nic
+  nsg_name      = var.nsg_name
+  vm_name       = var.vm_name
+  vm_size       = var.vm_size
+  vmcountnumber = var.vmcountnumber
+  public_ip_vm  = var.public_ip_vm
+}
