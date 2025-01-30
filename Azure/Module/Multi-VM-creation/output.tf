07829986905747e7aaea741a3e0c9e96eaf69058
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-31 00:52:42 +0530

    Create output.tf

---
+++ Azure/Module/Multi-VM-creation/output.tf
@@ -0,0 +1,33 @@
+output "resource_group_name" {
+  value = module.rg.resource_group_name
+}
+output "resource_group_location" {
+  value = module.rg.resource_group_location
+}
+output "virtualNet_name" {
+  value = module.rg.virtualNet_name
+}
+output "subnet" {
+  value = module.rg.subnet
+}
+output "address_space_details" {
+  value = module.rg.address_space_details
+}
+output "address_prefixes_details" {
+  value = module.rg.address_prefixes_details
+}
+output "nic_details" {
+  value = module.rg.nic_details
+}
+output "nsg_rule_details" {
+  value = module.rg.nsg_rule_details
+}
+output "vm_details" {
+  value = module.rg.vm_details
+}
+output "vm_size_details" {
+  value = module.rg.vm_size_details
+}
+output "public_ip_vm" {
+  value = module.rg.public_ip_vm
+}
