f1cb50efdb2689f0ef0623df32d7174eb5f4d7b5
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-31 01:42:19 +0530

    Update main.tf

--- Azure/Module/Multi-VM-creation/main.tf
+++ Azure/Module/Multi-VM-creation/main.tf
@@ -1,15 +1,21 @@
 module "rg" {
-  source        = "./module_rg"
-  rg_name       = var.rg_name
-  rg_location   = var.rg_location
-  vnet_name     = var.vnet_name
-  subnet_range  = var.subnet_range
-  subnet_name   = var.subnet_name
-  address_pre   = var.address_pre
-  nic           = var.nic
-  nsg_name      = var.nsg_name
-  vm_name       = var.vm_name
-  vm_size       = var.vm_size
-  vmcountnumber = var.vmcountnumber
-  public_ip_vm  = var.public_ip_vm
+  source            = "./module_rg"
+  rg_name           = var.rg_name
+  rg_location       = var.rg_location
+  vnet_name         = var.vnet_name
+  subnet_range      = var.subnet_range
+  subnet_name       = var.subnet_name
+  address_pre       = var.address_pre
+  nic               = var.nic
+  nsg_name          = var.nsg_name
+  vm_name           = var.vm_name
+  vm_size           = var.vm_size
+  vmcountnumber     = var.vmcountnumber
+  public_ip_vm      = var.public_ip_vm
+  nsg_rule_name     = var.nsg_rule_name
+  nsg_rule_priority = var.nsg_rule_priority
+  # source_ports      = var.source_ports
+  destination_ports  = var.destination_ports
+  direction_nsg     = var.direction_nsg
+  ngs_rule_define   = var.ngs_rule_define
 }
